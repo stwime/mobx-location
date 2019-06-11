d6e6fa1336d128adddd2d11736212f7bc5dd887c
Stefan <stwime@gmail.com>
2019-06-11 11:26:51 +0200

    Upgraded packages, not decoding URLs anymore

--- src/mobx-location.js
+++ src/mobx-location.js
@@ -1,6 +1,14 @@
-import { set, observable, action, autorun, toJS, observe, runInAction } from 'mobx'
-import queryString from 'query-string'
-import 'history-events'
+import {
+  set,
+  observable,
+  action,
+  autorun,
+  toJS,
+  observe,
+  runInAction,
+} from 'mobx';
+import queryString from 'query-string';
+import 'history-events';
 
 const propsToMirror = [
   'hash',
@@ -11,114 +19,112 @@ const propsToMirror = [
   'pathname',
   'port',
   'protocol',
-  'search'
-]
-const { location } = window
+  'search',
+];
+const { location } = window;
 
 export default ({ hashHistory, arrayFormat = 'bracket' }) => {
-  const createSnapshot = (previousQuery) => {
+  const createSnapshot = previousQuery => {
     const snapshot = propsToMirror.reduce((snapshot, prop) => {
-      snapshot[prop] = location[prop]
-      return snapshot
-    }, {})
-    let q
+      snapshot[prop] = location[prop];
+      return snapshot;
+    }, {});
+    let q;
 
     if (hashHistory) {
-      q = queryString.parse(snapshot.hash.split('?')[1], { arrayFormat })
+      q = queryString.parse(snapshot.hash.split('?')[1], { arrayFormat });
     } else {
-      q = queryString.parse(snapshot.search, { arrayFormat })
+      q = queryString.parse(snapshot.search, { arrayFormat });
     }
 
-    snapshot.query = q || {}
+    snapshot.query = q || {};
 
-    return snapshot
-  }
+    return snapshot;
+  };
 
-  const firstSnapshot = createSnapshot()
-  const locationObservable = observable(firstSnapshot)
+  const firstSnapshot = createSnapshot();
+  const locationObservable = observable(firstSnapshot);
 
-  const snapshotAndSet = action('changestateHandler', (ev) => {
-    const snapshot = createSnapshot(toJS(locationObservable.query))
-    const currentlyInObservable = toJS(locationObservable)
-    //unfortunately we need to check that the new snapshot is different-for example when integrating with angularjs it happens that angular router is setting a URL again after we changed it via interacting with observable
+  const snapshotAndSet = action('changestateHandler', ev => {
+    const snapshot = createSnapshot(toJS(locationObservable.query));
+    const currentlyInObservable = toJS(locationObservable);
+    // unfortunately we need to check that the new snapshot is different-for example when integrating with angularjs it happens that angular router is setting a URL again after we changed it via interacting with observable
 
     if (
       snapshot.href !== currentlyInObservable &&
-      decodeURI(snapshot.href) !== locationObservable.href
+      snapshot.href !== locationObservable.href
     ) {
-      set(locationObservable, snapshot)
+      set(locationObservable, snapshot);
     }
-  })
-  
+  });
+
   /**
    * executes each time a mobxLocation.query is mutated
    */
   const propagateQueryToLocationSearch = () => {
     const queryInObservable = queryString.stringify(
       toJS(locationObservable.query),
-      { encode: false, arrayFormat }
-    )
+      { encode: true, strict: false, arrayFormat }
+    );
     // console.log('currentlyInObservable: ', currentlyInObservable)
-    const { search, protocol, host, pathname, hash } = location
-    let qs = search
+    const { search, protocol, host, pathname, hash } = location;
+    let qs = search;
 
-    const hashParts = hash.split('?')
+    const hashParts = hash.split('?');
     if (hashHistory && hash.includes('?')) {
-      qs = hashParts[1]
+      qs = hashParts[1];
     }
     if (!qs && !queryInObservable) {
-      return
+      return;
     }
-    if (decodeURI(qs) === queryInObservable) {
-      return
+    if (qs === queryInObservable) {
+      return;
     }
     if (qs !== queryInObservable) {
-      let newUrl = protocol + '//' + host + pathname
+      let newUrl = `${protocol}//${host}${pathname}`;
       if (hashHistory) {
-        const newHash = hashParts[0] + '?' + queryInObservable
+        const newHash = `${hashParts[0]}?${queryInObservable}`;
 
-        locationObservable.hash = newHash
-        newUrl += newHash
+        locationObservable.hash = newHash;
+        newUrl += newHash;
       } else {
-        const newSearch = '?' + queryInObservable + hash
-        newUrl += newSearch
+        const newSearch = `?${queryInObservable}${hash}`;
+        newUrl += newSearch;
       }
-      runInAction(() => locationObservable.href = newUrl)
+      runInAction(() => (locationObservable.href = newUrl));
 
-      window.removeEventListener('changestate', snapshotAndSet)
+      window.removeEventListener('changestate', snapshotAndSet);
       // console.log('newUrl: ', newUrl)
-      history.replaceState(null, '', newUrl)
-      window.addEventListener('changestate', snapshotAndSet)
+      history.replaceState(null, '', newUrl);
+      window.addEventListener('changestate', snapshotAndSet);
     }
-  }
+  };
 
-  let unsubscribe = autorun(propagateQueryToLocationSearch)
+  let unsubscribe = autorun(propagateQueryToLocationSearch);
 
-
-
-  observe(locationObservable, (change) => {
-    const { name } = change
+  observe(locationObservable, change => {
+    const { name } = change;
     if (name === 'query') {
-      return // we ignore these
+      return; // we ignore these
     }
     if (location[change.name] !== change.newValue) {
-      const { search, protocol, host, pathname, hash } = locationObservable
-      const newUrl = protocol + '//' + host + pathname + search + hash
-      window.removeEventListener('changestate', snapshotAndSet)
+      const { search, protocol, host, pathname, hash } = locationObservable;
+      const newUrl = `${protocol}//${host}${pathname}${search}${hash}`;
+      window.removeEventListener('changestate', snapshotAndSet);
       if (change.name === 'search') {
-        unsubscribe()
+        unsubscribe();
 
         locationObservable.query = queryString.parse(change.newValue, {
-          arrayFormat
-        })
-        unsubscribe = autorun(propagateQueryToLocationSearch)
+          arrayFormat,
+        });
+        unsubscribe = autorun(propagateQueryToLocationSearch);
       }
-      history.pushState(null, '', newUrl)
-      window.addEventListener('changestate', snapshotAndSet)
+      history.pushState(null, '', newUrl);
+      window.addEventListener('changestate', snapshotAndSet);
     }
-  })
+  });
 
-  window.addEventListener('changestate', snapshotAndSet)
+  window.addEventListener('changestate', snapshotAndSet);
 
-  return locationObservable
-}
+  return locationObservable;
+};
